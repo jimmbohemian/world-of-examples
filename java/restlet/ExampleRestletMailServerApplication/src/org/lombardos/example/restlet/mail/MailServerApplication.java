8ce74a12bf79014e5b81d5a4dc63261f3b2cb04f
John Lombardo <john@lombardos.org>
2012-08-29 10:14:25 -0700

    ExampleRestletMailServerApplication 2.3.1

---
+++ java/restlet/ExampleRestletMailServerApplication/src/org/lombardos/example/restlet/mail/MailServerApplication.java
@@ -0,0 +1,34 @@
+package org.lombardos.example.restlet.mail;
+
+import org.restlet.Application;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.Restlet;
+import org.restlet.Server;
+import org.restlet.data.MediaType;
+import org.restlet.data.Protocol;
+
+public class MailServerApplication extends Application {
+
+	public static void main(String[] args) throws Exception {
+		Server mailServer = new Server(Protocol.HTTP,8111);
+		mailServer.setNext(new MailServerApplication());
+		mailServer.start();
+	}
+	
+	public Restlet createInboundRoot(){
+		return new Restlet() {
+
+			@Override
+			public void handle(Request request, Response response) {
+				StringBuilder sb = new StringBuilder();
+				sb.append("Method       : ").append(request.getMethod()).append('\n');
+				sb.append("Resource URI : ").append(request.getResourceRef()).append('\n');
+				sb.append("IP address   : ").append(request.getClientInfo().getAddress()).append('\n');
+				sb.append("Agent name   : ").append(request.getClientInfo().getAgentName()).append('\n');
+				sb.append("Agent version: ").append(request.getClientInfo().getAgentVersion()).append('\n');
+				response.setEntity(sb.toString(), MediaType.TEXT_PLAIN);
+			}
+		};
+	}
+}
